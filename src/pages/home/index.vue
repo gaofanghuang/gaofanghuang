b209dfba284db25c2e5c43cd5df8ff0c1e0d160c
Gaofang Huang <gaofanghuang@vip.qq.com>
2018-10-20 17:50:39 +0800

    简介

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,23 +1,113 @@
 <template>
   <div class="home-wrap">
-    <Nav></Nav>
+    <Nav />
+    <SNS />
+    <div class="home-about">
+      <div class="home-about-me">
+        <div class="home-about-avatar">
+          <img src="/static/img/carrot.jpg">
+        </div>
+        <h2 class="home-about-name">Huang GaoFang</h2>
+        <p>Front-end Developer & UI Designer</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Nav from './modules/nav'
-export default {
-  components: {
-    Nav
+  import Nav from './modules/nav'
+  import SNS from './modules/sns'
+  export default {
+    components: {
+      Nav,
+      SNS
+    }
   }
-}
 </script>
 
 <style lang="scss">
-.home-wrap {
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(90deg, rgba($green, 0.75), rgba($primary, 0.75));
-  color: #fff;
-}
+  .home-wrap {
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(90deg, rgba($green, 0.75), rgba($primary, 0.75));
+    color: #fff;
+
+    .home-about {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-shadow: 3px 6px 9px darken(rgba($primary, 0.75), 5%);
+    }
+
+    .home-about-me {
+      text-align: center;
+    }
+
+    .home-about-avatar {
+      width: px2rem(240);
+      height: px2rem(240);
+      border-radius: 50%;
+      background: url("/static/img/carrot_hover.jpg") no-repeat center center;
+      background-size: cover;
+      margin-left: auto;
+      margin-right: auto;
+      position: relative;
+      cursor: pointer;
+
+      &::before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: px2rem(240);
+        height: px2rem(240);
+        border-radius: 50%;
+        content: "";
+        border: 3px solid #fff;
+        opacity: 0.4;
+      }
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transition: 0.2s;
+        border-radius: 50%;
+        overflow: hidden;
+      }
+
+      &:hover {
+        img {
+          opacity: 0;
+        }
+
+        &::before {
+          animation: toWrap 0.28s ease-out;
+        }
+      }
+    }
+
+    @keyframes toWrap {
+      0% {
+        transform: scale(0.9);
+        opacity: 0.2;
+      }
+
+      50% {
+        transform: scale(1);
+        opacity: 1;
+      }
+
+      100% {
+        transform: scale(1.4);
+        opacity: 0;
+      }
+    }
+
+    .home-about-name {
+      margin-top: px2rem(64);
+      margin-bottom: px2rem(24);
+    }
+  }
 </style>
--- src/pages/home/modules/nav.vue
+++ src/pages/home/modules/nav.vue
@@ -21,8 +21,8 @@
 <style lang="scss">
   .nav {
     position: fixed;
-    right: px2rem(40);
-    top: px2rem(20);
+    right: px2rem(30);
+    top: px2rem(40);
 
     .nav-tap {
       display: block;
@@ -33,16 +33,19 @@
       cursor: pointer;
       position: relative;
       z-index: 99;
+      right: px2rem(20);
     }
 
     .nav-list {
-      display: none;
       position: fixed;
       right: 0;
-      top: 0;
-      background: rgba($primary, 0.75);
-      padding: px2rem(80) px2rem(40) px2rem(40) px2rem(40);
+      top: px2rem(100);
+      padding: 0 px2rem(40) px2rem(40) px2rem(40);
       z-index: 98;
+      text-align: center;
+      transition: transform .5s ease-in-out;
+      transform: scaleY(0);
+      transform-origin: 0 0;
     }
 
     a {
@@ -50,15 +53,33 @@
       font-size: px2rem(24);
       transition: 0.2s;
       opacity: 0.6;
+      position: relative;
+
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: -2px;
+        width: 100%;
+        height: 2px;
+        background: $primary;
+        transition: transform .5s ease-in-out;
+        transform: scaleX(0);
+        transform-origin: 100% 0;
+      }
 
       &:hover {
         opacity: 1;
+        &:before {
+          transform: scaleX(1);
+          transform-origin: 0 0;
+        }
       }
     }
 
     &:hover {
       .nav-list {
-        display: block;
+        transform: scaleY(1);
       }
     }
   }
